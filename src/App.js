d7fd6700c1a45c538568dd7f8261ad615017e368
Taehyeon Lee <tedlee@Taehyeons-MacBook-Pro.local>
2022-09-14 16:47:47 -0400

    added chaged to assignment 2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>This is branch for assignment 2</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
