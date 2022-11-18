3e37ef9f50a897d913c7a7b7bbdd7f9ea2ac63fc
Taehyeon Lee <tedlee@Taehyeons-MBP.fios-router.home>
2022-11-18 17:06:35 -0500

    explore page fixed

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -7,7 +7,7 @@ const PostSummaryList = () => {
     return(
         <ul className="list-group">
             {
-                postsArray.map(post =>
+                postsArray?.map(post =>
                     <PostSummaryItem key={post._id} post={post}/>
                 )
             }
